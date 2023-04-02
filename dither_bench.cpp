08c5b73506e6e639f0fc4f86d318a2f3f39b0c2d
undefinedDarkness <davidnoronha@outlook.in>
2023-04-02 22:54:05 +0530

    ++

--- dither_bench.cpp
+++ dither_bench.cpp
@@ -24,7 +24,7 @@ static pixelv *pixels;
 #endif
 
 #define CC(x)                                                                  \
-  (calcv) { (x >> 24) & 0xff, (x >> 16) & 0xff, (x >> 8) & 0xff, x & 0xff }
+  (calcv){ (x >> 24) & 0xff, (x >> 16) & 0xff, (x >> 8) & 0xff, x & 0xff }
 
 static std::vector<calcv> colors = {
     CC(0xc65f5fff), CC(0x859e82ff), CC(0xd9b27cff), CC(0x728797ff),
@@ -35,26 +35,26 @@ static std::vector<calcv> colors = {
 #include <cmath>
 #define sq(x) ((x) * (x))
 
+// TODO: Optimize
 pixelv findNearest(pixelv &find) {
-	int distance = INT_MAX;
-    calcv cc;
-	calcv init = __builtin_convertvector(find, calcv);
-    for (auto& color : colors) {
-		calcv c = (init-color); 
-		c = -c < c ? c : -c; 
-		const int res = c[0]+c[1]+c[2];
-        if (distance > res) {
-            cc = color;
-			distance = res;
-		}
+  int distance = INT_MAX;
+  calcv cc;
+  calcv init = __builtin_convertvector(find, calcv);
+  for (auto &color : colors) {
+    calcv c = (init - color);
+    c = -c < c ? c : -c;
+    const int res = c[0] + c[1] + c[2];
+    if (distance > res) {
+      cc = color;
+      distance = res;
     }
-	cc[3]=0xff;
-    return __builtin_convertvector(cc, pixelv);
+  }
+  return __builtin_convertvector(cc, pixelv);
 }
 
 // {{{
 
-void dither(size_t start_h, size_t height) {
+void ditherPass(size_t start_h, size_t height) {
   const size_t nPixels = width * height;
   size_t i;
   for (i = start_h; i < start_h + height - 3; i++) {
@@ -189,53 +189,126 @@ const char *txt;
 RL::Texture tex;
 RL::Image img;
 #include <atomic>
+#include <algorithm>
 
-std::atomic<bool> operationComplete { false };
+std::atomic<bool> operationComplete{false};
+
+pixelv lumaInvert(pixelv &c) {
+  const calcv max = _mm_set1_epi32(0xff);
+  const calcv zero = _mm_setzero_si128();
+  const int avg = 2*(c[0]+c[1]+c[2])/3;
+  calcv result = max - avg + __builtin_convertvector(c, calcv);
+  result = _mm_max_epi32(result, zero);
+  result = _mm_min_epi32(result, max);
+  return __builtin_convertvector(result,pixelv);
+}
 
 bool stopUpdating = false;
 void gameplay() {
   RL::BeginDrawing();
   if (!stopUpdating)
-	  tex = RL::LoadTextureFromImage(img);
+    tex = RL::LoadTextureFromImage(img);
   RL::ClearBackground(RL::WHITE);
   RL::DrawTexture(tex, 0, 0, RL::WHITE);
   RL::DrawText(txt, 0, 0, 30, RL::RED);
   RL::EndDrawing();
   if (operationComplete && !stopUpdating) {
-	  tex = RL::LoadTextureFromImage(img);
-	  stopUpdating = true;
+    tex = RL::LoadTextureFromImage(img);
+    stopUpdating = true;
   }
 }
 
-void colorize() {
-	size_t size = width*height;
-	size_t chunk = size/4;
-	if (chunk < 1000) {
-		for (int i = 0; i < size; i++)
-			pixels[i] = findNearest(pixels[i]);
-	} else {
-	size_t rem   = size&3;
-	std::array<std::thread, 5> threads;
-	size_t start = 0;
-	for (int i = 0; i < 4; i++) {
-		threads[i] = std::thread([=](){
-			for (int j = start; j < start+chunk; j++) {
-				pixels[j] = findNearest(pixels[j]);
-			}
-		});
-		start += chunk;
-	}
-	if (rem) {
-		threads[4] = std::thread([=](){
-			for (int j = start; j < start+rem; j++) {
-				pixels[j] = findNearest(pixels[j]);
-			}
-		});
-	}
-	for (int i = 0; i < 4+(!!rem); i++) {
-		threads[i].join();
-	}
-	}
+void colorPass() {
+  size_t size = width * height;
+  size_t chunk = size / 4;
+  if (chunk < 1000) {
+    for (int i = 0; i < size; i++)
+      pixels[i] = findNearest(pixels[i]);
+  } else {
+    size_t rem = size & 3;
+    std::array<std::thread, 5> threads;
+    size_t start = 0;
+    for (int i = 0; i < 4; i++) {
+      threads[i] = std::thread([=]() {
+        for (int j = start; j < start + chunk; j++) {
+          pixels[j] = findNearest(pixels[j]);
+        }
+      });
+      start += chunk;
+    }
+    if (rem) {
+      threads[4] = std::thread([=]() {
+        for (int j = start; j < start + rem; j++) {
+          pixels[j] = findNearest(pixels[j]);
+        }
+      });
+    }
+    for (int i = 0; i < 4 + (!!rem); i++) {
+      threads[i].join();
+    }
+  }
+}
+
+void lumaPass() {
+  size_t size = width * height;
+  size_t chunk = size / 4;
+  if (chunk < 1000) {
+    for (int i = 0; i < size; i++)
+      pixels[i] = lumaInvert(pixels[i]);
+  } else {
+    size_t rem = size & 3;
+    std::array<std::thread, 5> threads;
+    size_t start = 0;
+    for (int i = 0; i < 4; i++) {
+      threads[i] = std::thread([=]() {
+        for (int j = start; j < start + chunk; j++) {
+          pixels[j] = lumaInvert(pixels[j]);
+        }
+      });
+      start += chunk;
+    }
+    if (rem) {
+      threads[4] = std::thread([=]() {
+        for (int j = start; j < start + rem; j++) {
+          pixels[j] = lumaInvert(pixels[j]);
+        }
+      });
+    }
+    for (int i = 0; i < 4 + (!!rem); i++) {
+      threads[i].join();
+    }
+  }
+}
+
+void resetAlphaPass() {
+  size_t size = width * height;
+  size_t chunk = size / 4;
+  if (chunk < 1000) {
+    for (int i = 0; i < size; i++)
+      pixels[i][3] = 0xff;// = lumaInvert(pixels[i]);
+  } else {
+    size_t rem = size & 3;
+    std::array<std::thread, 5> threads;
+    size_t start = 0;
+    for (int i = 0; i < 4; i++) {
+      threads[i] = std::thread([=]() {
+        for (int j = start; j < start + chunk; j++) {
+          pixels[j][3] = 0xff;// = lumaInvert(pixels[j]);
+        }
+      });
+      start += chunk;
+    }
+    if (rem) {
+      threads[4] = std::thread([=]() {
+        for (int j = start; j < start + rem; j++) {
+          pixels[j][3]  = 0xff; //= lumaInvert(pixels[j]);
+        }
+      });
+    }
+    for (int i = 0; i < 4 + (!!rem); i++) {
+      threads[i].join();
+    }
+  }
 }
 
 int main(int argc, char **argv) {
@@ -249,28 +322,30 @@ int main(int argc, char **argv) {
   }
   RL::UnloadImagePalette(cols);
   RL::UnloadImage(f);
- 
-  img = RL::LoadImage("test2.png");
+
+  img = RL::LoadImage("g.png");
   RL::ImageFormat(&img, RL::PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
 
   width = img.width;
   height = img.height;
   pixels = static_cast<pixelv *>(img.data);
 
-	std::thread([&](){
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  auto start = std::chrono::high_resolution_clock::now();
+  // Main working thread
+  std::thread([&]() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    auto start = std::chrono::high_resolution_clock::now();
 
-  dither(0, img.height);
-  colorize();
+    // lumaPass();
+    //ditherPass(0, img.height);
+    colorPass(); // dog slow
+    //resetAlphaPass();
 
-  auto end = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
 
-  std::chrono::duration<double, std::milli> time = end - start;
-  txt = RL::TextFormat("Finished in %.5fms", time.count());
-	printf("%s\n",txt);
-	operationComplete = true;
-
+    std::chrono::duration<double, std::milli> time = end - start;
+    txt = RL::TextFormat("Finished in %.5fms", time.count());
+    printf("%s\n", txt);
+    operationComplete = true;
   }).detach();
 
   RL::InitWindow(img.width, img.height, "Preview");
